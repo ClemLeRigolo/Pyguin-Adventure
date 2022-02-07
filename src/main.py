956a083872e57e9e3762aca02c68236c3a4912ff
BrackBox <brackboxg10@gmail.com>
2022-02-07 11:39:38 +0100

    écran fullscreen

---
+++ src/main.py
@@ -0,0 +1,15 @@
+import pygame
+pygame.init()
+
+pygame.display.set_mode((1280,720), pygame.FULLSCREEN)
+pygame.display.set_caption("Pyco Park")
+
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+
+pygame.quit()
